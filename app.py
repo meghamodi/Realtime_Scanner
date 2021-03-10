b09e860e44ab7d36e8e17429abaae682decc8f29
meghamodi <meghamodi007@gmail.com>
2021-03-10 12:05:04 -0600

    first commit

--- app.py
+++ app.py
@@ -1,27 +1,60 @@
 import cv2
+import pyzbar
+import time
+
+import mysql.connector as mysql
 from pyzbar import pyzbar
 
+connection = mysql.connect(host="localhost",
+                     user="root",
+                     passwd="Modi123@",
+                     db="inventory")
+barcode_text = ""
 def read_barcodes(frame):
+    global barcode_text
     barcodes = pyzbar.decode(frame)
     for barcode in barcodes:
         x, y , w, h = barcode.rect
         barcode_text = barcode.data.decode('utf-8')
-        print(barcode_text)
+        print(type(barcode_text))
         cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
-    return frame
+    return frame, barcode_text
 
 def main():
+    global barcode_text
+    capture_duration = 30
     camera = cv2.VideoCapture(0)
     ret, frame = camera.read()
+    start_time = time.time()
     while ret:
         ret, frame = camera.read()
-        frame = read_barcodes(frame)
+        frame, barcode_text = read_barcodes(frame)
         cv2.imshow('Barcode reader', frame)
+        if len(barcode_text) > 0:
+            break
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
+
+
+
     camera.release()
     cv2.destroyAllWindows()
+    return barcode_text
+
 
 if __name__ == '__main__':
-    main()
+    # barcode_text = "sdfd"
+    barcode_text = main()
+    # print("here",barcode_text)
+    # name = 'Bai Antioxidant Infusion'
+    # mySql_insert_query = "INSERT INTO pantry (item_barcode,item_name) VALUES (%s, %s)"
+    # values = (barcode_text, name)
+    # mySql_insert_query = "INSERT INTO pantry (item_barcode,item_name) VALUES ({}, {});".format(barcode_text, name)
+    cursor = connection.cursor()
+    print(mySql_insert_query)
+    ## executing the query with values
+    cursor.execute(mySql_insert_query, values)
+    connection.commit()
+
+    print(cursor.rowcount, "record inserted")
---
+++ scraping.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import pandas as pd
+import mysql.connector as mysql
+import csv
+from app import main
+
+connection = mysql.connect(host="localhost",
+                     user="root",
+                     passwd="Modi123@",
+                     db="inventory")
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+driver.get("https://www.barcodelookup.com/")
+driver.implicitly_wait(4)
+table= driver.find_element_by_xpath("//*[@id='body-container']/section[1]/div/div[1]/p")
+text_area = driver.find_element_by_xpath("/html/body/div[1]/section[1]/div/div[2]/form/div/input")
+text_area.send_keys(main())
+product_id = main()
+driver.find_element_by_xpath("//*[@id='body-container']/section[1]/div/div[2]/form/div/span/button").click()
+
+driver.implicitly_wait(4)
+driver.get("https://www.barcodelookup.com/{}".format(product_id))
+product_details = driver.find_element_by_xpath("//*[@id='body-container']/section[2]/div/div/div[2]/h4")
+product = product_details.text
+
+mySql_insert_query = "INSERT INTO pantry (item_barcode,item_name) VALUES (%s, %s)"
+values = (product_id, product)
+# mySql_insert_query = "INSERT INTO pantry (item_barcode,item_name) VALUES ({}, {});".format(barcode_text, name)
+cursor = connection.cursor()
+print(mySql_insert_query)
+## executing the query with values
+cursor.execute(mySql_insert_query, values)
+connection.commit()
+
+print(cursor.rowcount, "record inserted")
